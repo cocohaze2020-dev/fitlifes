7e3678be4f9de9251e1233415bb5e3b85f67cf67
cocohaze2020-dev <cocohaze2020@gmail.com>
2026-05-19 19:52:22 +0800

    Add files via upload

---
+++ fitlife   index.html   about.html   services.html   gallery.html   contact.html   planner.html   style.css   script.js
@@ -0,0 +1,11 @@
+<header>
+  <h1>FitLife</h1>
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="services.html">Services</a>
+    <a href="gallery.html">Gallery</a>
+    <a href="contact.html">Contact</a>
+    <a href="planner.html">Planner</a>
+  </nav>
+</header>
